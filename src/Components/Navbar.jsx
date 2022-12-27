4c2f93ad1603eb6b9a71684d42bbb20d4f818d0f
ElcinQuluyev <elcin-quliyev-2022@mail.ru>
2022-12-28 00:12:59 +0400

    error cod change

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { ROUTES } from "../routes";
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <NavLink to={ROUTES.home}>Ana Sehife</NavLink>
+      <NavLink to={ROUTES.about}>Haqqinda</NavLink>
+      <NavLink to={ROUTES.contact}>Elaqeler</NavLink>
+    </nav>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+// export const ROUTES = [
+//   {
+//     path: "/home",
+//     text: "Ana Sehife",
+//     component: <Home />,
+//   },
+//   {
+//     path: "/about",
+//     text: "Haqqinda",
+//     component: <About />,
+//   },
+// ];
+export const ROUTES = {
+  home: "/home",
+  about: "/about",
+  contact: "/contact",
+};

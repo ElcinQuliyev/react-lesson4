eb96d2e4dd9a3f6bfb15610d2b93ac34371b6e80
ElcinQuluyev <elcin-quliyev-2022@mail.ru>
2022-12-28 00:54:32 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import React from "react";
-import Navbar from "./components/Navbar";
-import { ROUTES } from "./routes";
 import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./layout/Home";
+
+import About from "./layout/About.jsx";
+import Contact from "./layout/Contact.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { ROUTES } from "./routes";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Route path={ROUTES.home} element={<Home />} />
         <Route path={ROUTES.about} element={<About />} />
         <Route path={ROUTES.contact} element={<Contact />} />
+        {
+          //route verdikde id ve yunik key verilmelidir
+          /* key 0-dan baslaylr  */
+        }
       </Routes>
     </>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,6 +6,7 @@ import { ROUTES } from "../routes";
 export default function Navbar() {
   return (
     <nav className="nav">
+      {/* to= rooter deisdirilmesi ucun  */}
       <NavLink to={ROUTES.home}>Ana Sehife</NavLink>
       <NavLink to={ROUTES.about}>Haqqinda</NavLink>
       <NavLink to={ROUTES.contact}>Elaqeler</NavLink>
